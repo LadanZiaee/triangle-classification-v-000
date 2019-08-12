ada4b4268453b40bce4f0f70baf4fef9ed02f0cb
Giancarlo Zabaleta <gzabaleta48@gmail.com>
2019-08-12 18:04:29 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -7,23 +7,24 @@ class Triangle
     @c = c 
   end
   
-  def kind
-    fake_triangle = [(a + b < c), (a + c < b), (b + c < a)]
-    if fake_triangle
-      begin 
-        raise TriangleError
-      rescue TriangleError => error
-        puts error.message
-      end
-    elsif a == b && b == c
-    :equilateral
+    def kind
+    validate_triangle
+    if a == b && b == c
+      :equilateral
     elsif a == b || b == c || a == c
-    :isosceles
-    elsif a != b && b != c 
-    :scalene
+      :isosceles
+    else
+      :scalene
+    end
   end
-      
-  end 
+
+  def validate_triangle
+    real_triangle = [(a + b > c), (a + c > b), (b + c > a)]
+    [a, b, c].each do |side|
+      real_triangle << false if side <= 0 
+    raise TriangleError if real_triangle.include?(false)
+    end
+  end
   
   class TriangelError < StandardError
     def message
