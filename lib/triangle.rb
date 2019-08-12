439ec228eaf9de7a4bfa0b481d85b82472752976
Giancarlo Zabaleta <gzabaleta48@gmail.com>
2019-08-12 17:13:45 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,10 +1,15 @@
 class Triangle 
+  attr_accessor :a, :b, :c 
   
-  def initialize(one, two, three)
-    
+  def initialize(a, b, c)
+    @a = a 
+    @b = b
+    @c = c 
   end
   
   def kind 
     :equilateral, :isosceles. :scalene
-    if self.new = 
+    if self
+      
+  end 
 end
