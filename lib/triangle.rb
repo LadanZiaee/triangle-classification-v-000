dc2df93791c16304f1cc130e411b7517792c5ee8
Giancarlo Zabaleta <gzabaleta48@gmail.com>
2019-08-12 18:02:06 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -9,7 +9,7 @@ class Triangle
   
   def kind
     fake_triangle = [(a + b < c), (a + c < b), (b + c < a)]
-    if [a.b.c].each {|side| side <= 0}
+    if fake_triangle
       begin 
         raise TriangleError
       rescue TriangleError => error
