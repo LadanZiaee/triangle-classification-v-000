b8f4d35837945496dc7fc607827984942228b4e2
Giancarlo Zabaleta <gzabaleta48@gmail.com>
2019-08-12 18:07:07 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -7,27 +7,26 @@ class Triangle
     @c = c 
   end
   
-  def kind 
-    if a + b < c || b + c < a || a + c < b || a <= 0 || b <= 0 || c <= 0 
-      begin 
-        raise TriangleError
-      rescue TriangleError => error
-        puts error.message
-      end
-    elsif a == b && b == c
-    :equilateral
+   def kind
+    validate_triangle
+    if a == b && b == c
+      :equilateral
     elsif a == b || b == c || a == c
-    :isosceles
-    elsif a != b && b != c 
-    :scalene
+      :isosceles
+    else
+      :scalene
+    end
   end
-      
-  end 
-  
+
+  def validate_triangle
+    real_triangle = [(a + b > c), (a + c > b), (b + c > a)]
+    [a, b, c].each do |side|
+      real_triangle << false if side <= 0 
+    raise TriangleError if real_triangle.include?(false)
+    end
+  end
+
   class TriangleError < StandardError
-    def message
-      "triangles violating triangle inequality are illegal"
-    end 
   end
-  
+
 end
